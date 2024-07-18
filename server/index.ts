f61e730fef1a2305707ddc3dde0f4fb40bd58ef7
Nikita <kuzimovich8@gmail.com>
2024-07-18 14:12:45 +0300

    add server/register file 1

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,10 @@
 import controllers from './controllers';
+import register from './register';
 import routes from './routes';
 import services from './services';
 
 export default {
+  register,
   controllers,
   routes,
   services,
